b4720b8de6dd1f64572ff71899ca80a6f291c3d4
Jelani Canty <jelani.canty@peapoddigitallabs.com>
2021-05-17 18:21:45 -0500

    null checks;

--- service/yeet.js
+++ service/yeet.js
@@ -121,111 +121,139 @@ function clearTable(label) {
 
 function addPendingOrderInternal(orderId, clientId, printerId, designerId, description, fileSize, date, color) {
 
-    rowInput = [orderId, clientId, date, fileSize, color, printerId, designerId, description];
-    store.pendingOrders.push(rowInput);
+    if (orderId != null && clientId != null && printerId != null && designerId != null) {
 
-    var row = document.createElement('tr');
-    var tableBody = document.getElementById('pending-table-body');
-    for (let i = 0; i < rowInput.length; i++) {
-        cellInput = rowInput[i];
-        var cell = document.createElement('td');
-        if (i == 7) {
-            var child = document.createElement('textarea');
-            child.setAttribute('placeholder', cellInput);
-            child.setAttribute('readonly', '');
-            cell.appendChild(child);
-        } else {
-            cell.appendChild(document.createTextNode(cellInput));     
+        rowInput = [orderId, clientId, date, fileSize, color, printerId, designerId, description];
+        store.pendingOrders.push(rowInput);
+    
+        var row = document.createElement('tr');
+        var tableBody = document.getElementById('pending-table-body');
+        for (let i = 0; i < rowInput.length; i++) {
+            cellInput = rowInput[i];
+            var cell = document.createElement('td');
+            if (i == 7) {
+                var child = document.createElement('textarea');
+                child.setAttribute('placeholder', cellInput);
+                child.setAttribute('readonly', '');
+                cell.appendChild(child);
+            } else {
+                cell.appendChild(document.createTextNode(cellInput));     
+            }
+            row.appendChild(cell);
+    
         }
-        row.appendChild(cell);
+        row.append(createRadio(String(orderId), 'approved'));
+        row.append(createRadio(String(orderId), 'denied'));
+        tableBody.appendChild(row);
+        //saveLocalStorage();
 
+    } else {
+        console.log('One of the following required fields is null: orderId, clientId, printerId, designerId');
     }
-    row.append(createRadio(String(orderId), 'approved'));
-    row.append(createRadio(String(orderId), 'denied'));
-    tableBody.appendChild(row);
-    //saveLocalStorage();
 
 }
 
 function addOrderInternal(orderId, clientId, printerId, designerId, description, date, color) {
-    rowInput = [orderId, clientId, date, color, printerId, designerId, description];
-    store.orders.push(rowInput);
 
-    var row = document.createElement('tr');
-    var tableBody = document.getElementById('accepted-table-body');
-    for (let i = 0; i < rowInput.length; i++) {
-        cellInput = rowInput[i];
-        var cell = document.createElement('td');
-        if (i == 6) {
-            var child = document.createElement('textarea');
-            child.setAttribute('placeholder', cellInput);
-            child.setAttribute('readonly', '');
-            cell.appendChild(child);
-        } else {    
-            cell.appendChild(document.createTextNode(cellInput));
+    if (orderId != null && clientId != null && printerId != null && designerId != null) {
+
+        rowInput = [orderId, clientId, date, color, printerId, designerId, description];
+        store.orders.push(rowInput);
+
+        var row = document.createElement('tr');
+        var tableBody = document.getElementById('accepted-table-body');
+        for (let i = 0; i < rowInput.length; i++) {
+            cellInput = rowInput[i];
+            var cell = document.createElement('td');
+            if (i == 6) {
+                var child = document.createElement('textarea');
+                child.setAttribute('placeholder', cellInput);
+                child.setAttribute('readonly', '');
+                cell.appendChild(child);
+            } else {    
+                cell.appendChild(document.createTextNode(cellInput));
+            }
+            row.appendChild(cell);
         }
-        row.appendChild(cell);
+        const selector = createSelector(String(orderId), 'status');
+        row.append(selector);
+        tableBody.appendChild(row);
+        //saveLocalStorage();
+
+
+    } else {
+        console.log('One of the following required fields is null: orderId, clientId, printerId, designerId');
     }
-    const selector = createSelector(String(orderId), 'status');
-    row.append(selector);
-    tableBody.appendChild(row);
-    //saveLocalStorage();
 
 }
 
 function addDeclinedOrderInternal(orderId, clientId, printerId, designerId, description, date, color) {
-    rowInput = [orderId, clientId, date, printerId, designerId, color, description];
-    store.declinedOrders.push(rowInput);
 
-    var row = document.createElement('tr');
-    var tableBody = document.getElementById('declined-table-body');
-    for (let i = 0; i < rowInput.length; i++) {
-        cellInput = rowInput[i];
-        var cell = document.createElement('td');
-        if (i== 6){
-            var child = document.createElement('textarea');
-            child.setAttribute('placeholder', cellInput);
-            child.setAttribute('readonly', '');
-            cell.appendChild(child);
+    if (orderId != null && clientId != null && printerId != null && designerId != null) {
 
-        } else {
-            cell.appendChild(document.createTextNode(cellInput)); 
+        rowInput = [orderId, clientId, date, printerId, designerId, color, description];
+        store.declinedOrders.push(rowInput);
+
+        var row = document.createElement('tr');
+        var tableBody = document.getElementById('declined-table-body');
+        for (let i = 0; i < rowInput.length; i++) {
+            cellInput = rowInput[i];
+            var cell = document.createElement('td');
+            if (i== 6){
+                var child = document.createElement('textarea');
+                child.setAttribute('placeholder', cellInput);
+                child.setAttribute('readonly', '');
+                cell.appendChild(child);
+
+            } else {
+                cell.appendChild(document.createTextNode(cellInput)); 
+            }
+            row.appendChild(cell);
+            if (i == 2) {
+                var reasonCell = document.createElement('td');
+                var child = document.createElement('input');
+                child.setAttribute('placeholder', 'Type Reason Here');
+                child.setAttribute('id', createInputId(orderId, 'decline'));
+                reasonCell.appendChild(child);
+                row.appendChild(reasonCell);
+            } 
         }
-        row.appendChild(cell);
-        if (i == 2) {
-            var reasonCell = document.createElement('td');
-            var child = document.createElement('input');
-            child.setAttribute('placeholder', 'Type Reason Here');
-            child.setAttribute('id', createInputId(orderId, 'decline'));
-            reasonCell.appendChild(child);
-            row.appendChild(reasonCell);
-        } 
+        tableBody.appendChild(row);
+        //saveLocalStorage();
+
+    } else {
+        console.log('One of the following required fields is null: orderId, clientId, printerId, designerId');
     }
-    tableBody.appendChild(row);
-    //saveLocalStorage();
 }
 
 function addFinishedOrderInternal(orderId, clientId, printerId, designerId, description, date, color, action, reason) {
-    rowInput = [orderId, clientId, date, action, reason, printerId, designerId, color, description];
 
-    var row = document.createElement('tr');
-    var tableBody = document.getElementById('history-table-body');
-    for (let i = 0; i < rowInput.length; i++) {
-        cellInput = rowInput[i];
-        var cell = document.createElement('td');
-        if (i==4 || i==8){
-            var child = document.createElement('textarea');
-            child.setAttribute('placeholder', cellInput);
-            child.setAttribute('readonly', '');
-            cell.appendChild(child);
+    if (orderId != null && clientId != null && printerId != null && designerId != null) {
 
-        } else {
-            cell.appendChild(document.createTextNode(cellInput)); 
+        rowInput = [orderId, clientId, date, action, reason, printerId, designerId, color, description];
+
+        var row = document.createElement('tr');
+        var tableBody = document.getElementById('history-table-body');
+        for (let i = 0; i < rowInput.length; i++) {
+            cellInput = rowInput[i];
+            var cell = document.createElement('td');
+            if (i==4 || i==8){
+                var child = document.createElement('textarea');
+                child.setAttribute('placeholder', cellInput);
+                child.setAttribute('readonly', '');
+                cell.appendChild(child);
+
+            } else {
+                cell.appendChild(document.createTextNode(cellInput)); 
+            }
+            row.appendChild(cell);
         }
-        row.appendChild(cell);
+        tableBody.appendChild(row);
+        //saveLocalStorage();
+
+    } else {
+        console.log('One of the following required fields is null: orderId, clientId, printerId, designerId');
     }
-    tableBody.appendChild(row);
-    //saveLocalStorage();
 }
 
 function createRadio(id, label) {
